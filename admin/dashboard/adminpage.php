a595c95679b2057eae0b6677da8e3362a927a264
MizzoCotV1 <panduramaditya15@gmail.com>
2023-08-30 14:08:37 +0700

    upload

---
+++ admin/dashboard/adminpage.php
@@ -0,0 +1,16 @@
+<?php
+            require_once("conn.php");
+            if ($_SESSION['hak_akses'] !== 'admin') {
+                echo "<script>
+                    document.location.href='http://localhost/terput2/admin/dashboard/';
+                </script>";
+                echo '<script>
+                    document.addEventListener("DOMContentLoaded", function() {
+                        var elements = document.getElementsByClassName("admin"); // Replace with the actual class name
+                        for (var i = 0; i < elements.length; i++) {
+                            elements[i].classList.add("d-none");
+                        }
+                    });
+                </script>';
+            }
+?>
---
+++ admin/dashboard/alertsweet.js
@@ -0,0 +1,29 @@
+
+    function usernameexist(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Username sudah dipakai!',
+        })
+    };
+    function formkosong(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Form kosong',
+        })
+    };
+    function cpassworderror(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Password tidak sama!',
+        })
+    };
+    function kesalahan(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Terjadi Kesalahan',
+        })
+    };
